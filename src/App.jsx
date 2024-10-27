99d6ef432acfd498c46243f77eb967392b4bbdb1
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-27 13:33:34 +0600

    navber

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
+import Nevber from './Compontent/Navber/Nevber'
 
 function App() {
 
   return (
     <>
+    <Nevber></Nevber>
       <div className="diff aspect-[16/9] h-[70vh]">
         <div className="diff-item-1">
           <img alt="daisy" src="https://img.daisyui.com/images/stock/photo-1560717789-0ac7c58ac90a.webp" />
