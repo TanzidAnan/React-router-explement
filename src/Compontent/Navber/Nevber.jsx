40069238cb6b71f98cb388ce09c2741a3b0a375d
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-27 14:15:18 +0600

    navber toggle

--- src/Compontent/Navber/Nevber.jsx
+++ src/Compontent/Navber/Nevber.jsx
@@ -1,20 +1,32 @@
+import { useState } from "react";
+import Link from "../Link/Link";
+import { TiThMenu } from "react-icons/ti";
+import { IoCloseSharp } from "react-icons/io5";
+
+
 
 const Nevber = () => {
+    const [open, setOpen] = useState(false)
     const routes = [
         { id: 1, path: "/", name: "Home" },
         { id: 2, path: "/about", name: "About" },
         { id: 3, path: "/services", name: "Services" },
         { id: 4, path: "/contact", name: "Contact" },
         { id: 5, path: "/profile", name: "Profile" }
-      ];
-      
+    ];
+
     return (
         <nav>
+            <div className="md:hidden text-3xl" onClick={() => setOpen(!open)}>
+                {
+                    open===true?<IoCloseSharp></IoCloseSharp>:<TiThMenu></TiThMenu>
+                }
+
+            </div>
             <ul className="flex-wrap md:flex ml-6 md:ml-0 justify-around py-4 text-orange-500">
-            {
-                routes.map(route =><li key={route.id}>
-                    <a href={`${route.path}`}>{route.name}</a></li>)
-            }
+                {
+                    routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
             </ul>
         </nav>
     );
